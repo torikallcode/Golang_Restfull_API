542469314c8ba157b0ff9b18279110a1ffc9bece
torikallcode <torikalcontent@gmail.com>
2025-02-05 19:46:08 +0800

    create handler GetUser

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,35 +1,33 @@
+package handlers
 
+import (
+	"encoding/json"
+	"golang_restfull_API/models"
+	"net/http"
+)
 
+var users []models.User
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
 	// set header response sebagi json
+	w.Header().Set("Content-Type", "application/json")
 	// mengubah slice users ke json dan kirim sebagai response
+	json.NewEncoder(w).Encode(users)
+}
 
-	// set header response sebagai json
-	// ambil parameter url (/users/{id})
-	// konversi string id ke int
-	// cari user berdasarkan id
+// set header response sebagai json
+// ambil parameter dari url
+// konversi string id menjadi int
+// cari dan update user
+// hapus user lama dari slice
+// ambil data baru
+// pertahankan id yang sama
+// tambahkan user yang sudah di update
+// kirim user yang sudah diupdate
 
-	// kirim user yang ditemukan jika tidak ditemukan maka kembalikan "not found"
-
-	// set header response sebagai json
-	// siapkan variable untuk menampung input
-	// decode body request json ke struct user
-	// generate ID sederhana dari slice users
-	// tambahkan user yang baru dibuat
-	// kirim user yang sudah ditambahkan
-
-	// set header response sebagai json
-	// ambil parameter dari url
-	// konversi string id menjadi int
-	// cari dan update user
-	// hapus user lama dari slice
-	// ambil data baru
-	// pertahankan id yang sama
-	// tambahkan user yang sudah di update
-	// kirim user yang sudah diupdate
-
-	// set header response sebagai json
-	// ambil parameter dari url
-	// konversi string id menjadi int
-	// cari dan hapus user
-	// hapus user dari slice
-	// http.error status no content
+// set header response sebagai json
+// ambil parameter dari url
+// konversi string id menjadi int
+// cari dan hapus user
+// hapus user dari slice
+// http.error status no content
