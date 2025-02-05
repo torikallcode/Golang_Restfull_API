18085c54969ea976a33b29e289a973996bc606a6
torikallcode <torikalcontent@gmail.com>
2025-02-05 19:47:14 +0800

    create handler GetUser

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"golang_restfull_API/models"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 var users []models.User
@@ -13,6 +16,28 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// mengubah slice users ke json dan kirim sebagai response
 	json.NewEncoder(w).Encode(users)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	// set header response sebagai json
+	w.Header().Set("Content-Type", "application/json")
+	// ambil parameter url (/users/{id})
+	params := mux.Vars(r)
+	// konversi string id ke int
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid user", http.StatusBadRequest)
+		return
+	}
+	// cari user berdasarkan id
+	for _, item := range users {
+		if item.ID == id {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	// kirim user yang ditemukan jika tidak ditemukan maka kembalikan "not found"
+	http.Error(w, "user not found", http.StatusNotFound)
 }
 
 // set header response sebagai json
