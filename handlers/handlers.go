296e144d4a0ca145cf6eb9ada87fc159f01ba95a
torikallcode <torikalcontent@gmail.com>
2025-02-05 19:34:17 +0800

    first commit

---
+++ handlers/handlers.go
@@ -0,0 +1,35 @@
+
+
+	// set header response sebagi json
+	// mengubah slice users ke json dan kirim sebagai response
+
+	// set header response sebagai json
+	// ambil parameter url (/users/{id})
+	// konversi string id ke int
+	// cari user berdasarkan id
+
+	// kirim user yang ditemukan jika tidak ditemukan maka kembalikan "not found"
+
+	// set header response sebagai json
+	// siapkan variable untuk menampung input
+	// decode body request json ke struct user
+	// generate ID sederhana dari slice users
+	// tambahkan user yang baru dibuat
+	// kirim user yang sudah ditambahkan
+
+	// set header response sebagai json
+	// ambil parameter dari url
+	// konversi string id menjadi int
+	// cari dan update user
+	// hapus user lama dari slice
+	// ambil data baru
+	// pertahankan id yang sama
+	// tambahkan user yang sudah di update
+	// kirim user yang sudah diupdate
+
+	// set header response sebagai json
+	// ambil parameter dari url
+	// konversi string id menjadi int
+	// cari dan hapus user
+	// hapus user dari slice
+	// http.error status no content
